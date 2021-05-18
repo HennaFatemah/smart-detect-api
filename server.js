2f935f226363acd18c4d479f640352e6f5c01226
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-18 18:44:56 -0400

    put endpoint created and tested

--- server.js
+++ server.js
@@ -65,6 +65,21 @@ app.get('/profile/:id', (req, res)=>{
     }
 })
 
+app.put('/image', (req, res)=>{
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            user.entries++
+            return res.json(user.entries)
+        }
+    })
+    if(!found){
+        res.status(404).json('user does not match')
+    }
+})
+
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
 })
