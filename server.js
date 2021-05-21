36929b8a46be4c617219aabb645cd9015514016f
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-21 05:17:45 -0400

    uregister post endpoint connected to db

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const knex = require('knex');
 
-const postgres = knex({
+const db = knex({
     client: 'pg',
     connection: {
         host: '127.0.0.1',
@@ -13,7 +13,10 @@ const postgres = knex({
     }
 })
 
-postgres.select('*').from('users');
+db.select('*').from('users')
+    .then(data => {
+        console.log(data);
+    })
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
@@ -72,15 +75,11 @@ app.post('/register', (req, res)=> {
     // bcrypt.hash(password, null, null, function(err, hash) {
     //     console.log(hash);
     // });
-    database.users.push(
-        {
-            id: 125,
-            name: name,
-            email: email,
-            entries: 0,
-            joined: new Date()
-        }
-    )
+    db('users').insert({
+        email: email,
+        name: name,
+        joined: new Date()
+    }).then(console.log);
     res.json(database.users[database.users.length-1]);
 })
 
