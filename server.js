6d669de66bab32a24b9502520ff2b31dcf2cf20d
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-22 05:55:44 -0400

    updating database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-amorphous-13644',
         user:'hennafatemah',
         password: 'rootroot',
         database: 'smart-detect'
